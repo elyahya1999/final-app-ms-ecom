356990561c603a482806f4569c98241f2bf71fba
Yahya ELMALKI <yahya.elmalki09@gmail.com>
2024-11-27 13:12:21 +0100

    ajout de tout les ms avec le config service

--- customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -4,6 +4,7 @@ import org.sid.customerservice.entities.Customer;
 import org.sid.customerservice.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
